1865a371d6c8efb26e5788719ad9b86a2379923f
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-07-09 19:48:47 +0800

    Update DataTable.js
    OOP

--- client/src/components/DataTable.js
+++ client/src/components/DataTable.js
@@ -1,59 +1,73 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import LastUpdate from "./LastUpdate";
 import { MDBTable, MDBTableHead, MDBTableBody, MDBMedia } from "mdbreact";
 
-const DataTable = () => {
-  
-  const [datatable, setDatatable] = useState([]);
-  const getData = async () => {
+class DataTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datatable: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://192.168.1.109:5000/attendance')
+      .then(res => res.json())
+      .then(result => {
+        this.setState({
+          isLoaded: true,
+          datatable: result
+        });
+      });
+  }
+
+  fetchImage = (path) => {
     try {
-      const response = await fetch("http://192.168.1.109:5000/attendance");
-      const jsonData = await response.json();
-      setDatatable(jsonData);
+      return require('./images/' + path);
     } catch (err) {
-      console.error(err.message);
+      return 'image not found';
     }
   };
 
-  useEffect(() => {
-    getData();
-  }, []);
-
-
-  const getImage = (path) => {
-    return require('./images/' + path);
-  };
-  
-
-  return (
-    <Fragment>
+  render() {
+    
+    const { datatable } = this.state;
+    console.log (datatable);
+    if (!this.state.isLoaded) {
+      return <div>  Loading ... </div>;
+    } else {
+      return (
+        <Fragment>
       <br/>
-      <MDBTable hover className="table mt-5 text-center" >
-      <MDBTableHead>
-          <tr>
-            <th>bird_id</th>
-            <th>aviary_id</th>
-            <th>weight</th>
-            <th>image_path</th>
+       <MDBTable hover className="table mt-5 text-center" >
+       <MDBTableHead>
+           <tr>
+             <th>bird_id</th>
+             <th>aviary_id</th>
+             <th>weight</th>
+             <th>image_path</th>
             <th>time_stamp</th> 
-          </tr>
-        </MDBTableHead>
+           </tr>
+         </MDBTableHead>
         <MDBTableBody>
-          {datatable.map(datatable => (
+           {datatable.map(datatable => (
             
             <tr key={datatable.time_stamp}>
               <td> {datatable.bird_id}</td>
               <td> {datatable.aviary_id}</td>
               <td> {datatable.weight}</td>
-              <td> <MDBMedia object src = {getImage(datatable.image_path)} 
+              <td> <MDBMedia object src = {this.fetchImage(datatable.image_path)} 
                   alt = {datatable.image_path} width="80" height="80"/></td>
               <td> {datatable.time_stamp}</td>
             </tr>
           ))}             
         </MDBTableBody></MDBTable><LastUpdate/>
     </Fragment>
+      );
+    }
     
-  );
-};
+  }
+}
 
 export default DataTable;
